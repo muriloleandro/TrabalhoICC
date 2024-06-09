c866ac44a1c13a68b5e7bc3ff9508cbab8ddb7aa
jonaskaita <jonaskaita@gmail.com>
2024-06-08 21:36:34 -0300

    Update main.c
    :P

--- main.c
+++ main.c
@@ -40,8 +40,8 @@ void modificar_reserva(Dados *dados) {
     char cpf_mod[15];
     char *assento_mod;
     scanf("%s %s %s %s %s", cpf_consulta_mod, nome_mod, sobre_mod, cpf_mod, assento);
-    for(int i = 0; i < dados->num_reservas; i++) {
-        if(strcmp(dados->reservas[i].cpf, cpf_consulta_mod) == 0 ) {
+    for (int i = 0; i < dados->num_reservas; i++) {
+        if (strcmp(dados->reservas[i].cpf, cpf_consulta_mod) == 0 ) {
             dados->reservas[i].cpf = cpf_mod;
             dados->reservas[i].nome = nome_mod;
             dados->reservas[i].sobre = sobre_mod;
@@ -52,20 +52,20 @@ void modificar_reserva(Dados *dados) {
             printf("%s/%s/%s\n", dados->reservas[i].data_da_viagem.dia, dados->reservas[i].data_da_viagem.mes, dados->reservas[i].data_da_viagem.ano);
             printf("Voo: %s\n", dados->numero_do_voo);
             printf("Assento: %s\n", dados->reserva[i].numero_assento);
-            if(dados->reservas[i].classe == 0) {
+            if (dados->reservas[i].classe == 0) {
                 printf("Classe: economica\n");
             }
             else {
                 printf("Classe: executiva");
             }
             printf("Trecho: %s %s\n", dados->origem, dados->destino);
-            if(dados->reservas[i].classe == 0) {
+            if (dados->reservas[i].classe == 0) {
                 printf("Valores: %s\n", dados->valores[0]);
             }
             else {
                 printf("Valores: %s\n", dados->valores[1]);
             }
-            for(int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {
                 printf("-----");
             }
             printf("\n");
@@ -78,9 +78,9 @@ void modificar_reserva(Dados *dados) {
 void cancelar_reserva(Dados *dados) {
     char cancelado[15];
     scanf("%s", cancelado);
-    for(int i = 0; i < dados->num_reservas; i++){
-        if(strcmp(dados->reservas[i].cpf, cancelado) == 0){
-            for(int j = i; j < dados->num_reservas - 1; j++){
+    for (int i = 0; i < dados->num_reservas; i++){
+        if (strcmp(dados->reservas[i].cpf, cancelado) == 0){
+            for (int j = i; j < dados->num_reservas - 1; j++){
                 dados->reservas[j] = dados->reservas[j + 1];
             }
             dados->num_reservas = dados->num_reservas - 1;
@@ -90,14 +90,14 @@ void cancelar_reserva(Dados *dados) {
             printf("%s\n", dados->reservas[i].data_da_viagem);
             printf("Voo: %s\n", dados->numero_do_voo);
             printf("Assento: %s\n", dados->reservas[i].numero_assento);
-            if(dados->reservas[i].classe == 0) {
+            if (dados->reservas[i].classe == 0) {
                 printf("Classe: economica\n");
             }
             else {
                 printf("Classe: executiva\n");
             }
             printf("Trecho: %s %s\n", dados->origem, dadors->destino);
-            if(dados->reservas[i].classe == 0) {
+            if (dados->reservas[i].classe == 0) {
                 printf("Valor: %s\n", dados->valores[0]);
             }
             else {
@@ -106,7 +106,7 @@ void cancelar_reserva(Dados *dados) {
             break;
         }
     }
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++){
         printf("-----");
     }
     printf("\n");
@@ -116,7 +116,7 @@ void fechar_dia(Dados dados) {
     float posicao = 0;
     printf("Fechamento do dia: \n");
     printf("Quantidade de reservas: %d\n", dados.num_reservas);
-    for(int i = 0; i < dados.num_reservas; i++) {
+    for (int i = 0; i < dados.num_reservas; i++) {
         if (dados.reservas[i].classe == 0) {
             posicao += dados.valores[0];
         }
